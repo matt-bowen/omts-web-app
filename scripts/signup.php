ecc173706ae5d610204f7b53d7ca00a3390366ab
matt-bowen <mt.bowen@hotmail.com>
2018-03-26 00:00:45 -0400

    database access and insertion via php

--- scripts/signup.php
+++ scripts/signup.php
@@ -1,29 +1,48 @@
-<!DOCTYPE html>
-<head><title>Signup Landing Page</title></head>
+ <!DOCTYPE html>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../styles/bootstrap.min.css">
+    <!-- icon -->
+    <link rel="shortcut icon" href="../images/favicon.ico">
+
+    <title>Signup Landing Page</title>
+</head>
 <body>
 <?php
-	phpinfo();
-    //if (isset($_POST[submit])) {
-      //  echo '<h1>Hello</h1>';
-    // }
-    //$servername = "localhost";
-    //$username = "root";
-    //$password = "";
-    //$dbname = "OMTS";
+	//phpinfo();
+    if (isset($_POST['submitbutton'])) {
+        
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "OMTS";
 
     // Create connection
-    //$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
-    //if ($conn->connect_error) {
-      //  die("Connection failed: " . $conn->connect_error);
-    //} 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
 
-    //$sql = "SELECT acct_num FROM customers ORDER BY acct_num DESC LIMIT 1";
-    //$result = $conn->query($sql);
+
+    $sql = "SELECT acct_num FROM customer ORDER BY acct_num DESC LIMIT 1";
+    $result = $conn->query($sql);
+    if ($row = mysqli_fetch_row($result)) {
+        $sql = "INSERT INTO customer (acct_num, f_name, l_name) VALUES ($row[0]+1, '$_POST[firstname]', '$_POST[lastname]')";
+        $conn->query($sql);
+        if ($conn->connect_error) {
+            die("Insertion failed: " . $conn->connect_error);
+        } 
+        echo "<p>Data (possibly) inserted</p>";
+    }
     
     
-    // $sql = "INSERT INTO customers (acct_num, f_name, l_name) VALUES (($result+1), '$_POST[firstname]', '$_POST[lastname]')";
-    // $conn->query($sql);
+    
+    }
 ?>
 
 </body>
