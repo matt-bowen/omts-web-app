d5d3ee278cfcd8f656713dd44e09224457d62ba9
matt-bowen <mt.bowen@hotmail.com>
2018-03-25 23:26:34 -0400

    re-add php to signup.php

--- scripts/signup.php
+++ scripts/signup.php
@@ -1,8 +1,29 @@
 <!DOCTYPE html>
-<head>Hello World</head>
+<head><title>Signup Landing Page</title></head>
 <body>
 <?php
 	phpinfo();
+    //if (isset($_POST[submit])) {
+      //  echo '<h1>Hello</h1>';
+    // }
+    //$servername = "localhost";
+    //$username = "root";
+    //$password = "";
+    //$dbname = "OMTS";
+
+    // Create connection
+    //$conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    //if ($conn->connect_error) {
+      //  die("Connection failed: " . $conn->connect_error);
+    //} 
+
+    //$sql = "SELECT acct_num FROM customers ORDER BY acct_num DESC LIMIT 1";
+    //$result = $conn->query($sql);
+    
+    
+    // $sql = "INSERT INTO customers (acct_num, f_name, l_name) VALUES (($result+1), '$_POST[firstname]', '$_POST[lastname]')";
+    // $conn->query($sql);
 ?>
 
 </body>
