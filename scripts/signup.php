3ca9c9328040da23318cdb93f01853783855bf40
matt-bowen <mt.bowen@hotmail.com>
2018-03-26 01:36:37 -0400

    Full signup page with database insertions

--- scripts/signup.php
+++ scripts/signup.php
@@ -15,30 +15,33 @@
 <?php
 	//phpinfo();
     if (isset($_POST['submitbutton'])) {
-        
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "OMTS";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
+        $cc_exp = $_POST['ccexp_month'] . $_POST['ccexp_year'];
 
 
-    $sql = "SELECT acct_num FROM customer ORDER BY acct_num DESC LIMIT 1";
-    $result = $conn->query($sql);
-    if ($row = mysqli_fetch_row($result)) {
-        $sql = "INSERT INTO customer (acct_num, f_name, l_name) VALUES ($row[0]+1, '$_POST[firstname]', '$_POST[lastname]')";
-        $conn->query($sql);
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "OMTS";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
         if ($conn->connect_error) {
-            die("Insertion failed: " . $conn->connect_error);
+            die("Connection failed: " . $conn->connect_error);
         } 
-        echo "<p>Data (possibly) inserted</p>";
-    }
+
+
+        $sql = "SELECT acct_num FROM customer ORDER BY acct_num DESC LIMIT 1";
+        $result = $conn->query($sql);
+        if ($row = mysqli_fetch_row($result)) {
+            //$sql = "INSERT INTO customer (acct_num, f_name, l_name, street_num, street_name, city, prov, postal, phone_num, email, password, cc_num, cc_exp) VALUES ($row[0]+1, '$_POST[firstname]', '$_POST[lastname]', '$_POST[streetnum]', '$_POST[streetname]', '$_POST[city]', '$_POST[province]', '$_POST[postal]', '$_POST[phone]', '$_POST[email]', '$_POST[password]', '$_POST[cc]', $cc_exp)";
+            $sql = "INSERT INTO customer VALUES ($row[0]+1, '$_POST[password]', '$_POST[firstname]', '$_POST[lastname]', '$_POST[streetnum]', '$_POST[streetname]', '$_POST[city]', '$_POST[province]', '$_POST[postal]', '$_POST[phone]', '$_POST[email]', '$_POST[cc]', $cc_exp)";
+            $conn->query($sql);
+            if ($conn->connect_error) {
+                die("Insertion failed: " . $conn->connect_error);
+            } 
+            echo "<p>Data (possibly) inserted: " . mysqli_error($conn) . "</p>";
+        }
     
     
     
